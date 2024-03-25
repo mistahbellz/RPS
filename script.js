795710a827a7f0d6dd83e04887431107c10d1610
mistahbellz <mistahbellz@bellzy007@yahoo.com>
2024-03-25 22:28:03 +0300

    added compare function to decide the overall winner

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let refMsg = document.getElementById("refMsg"); //message from the Games Judge
 let winnerMsg = document.getElementById("winner"); //Winner!
 let p1ChoiceLabel = ""; 
 let cpuChoiceLabel = "";
-let gameOn = false;
+let gameOn = true;
 refMsg.textContent = "Welcome! Click on Start Game to begin";
 winnerMsg.textContent = "";
 
@@ -23,64 +23,68 @@ PC= {
 pcChoiceArray = ["Rock", "Paper", "Scissor"];
 
 function Player1Chose(event) {
-  const buttonId = event.target.id;
-  let result;
-
-  switch (buttonId) {
-      case 'rockBtn':
-          result = "Rock";
-          break;
-      case 'paperBtn':
-          result = "Paper";
-          break;
-      case 'scissorsBtn':
-          result = "Scissor";;
-          break;
-      default:
-          result = null;
-          break;
-  }
-
-  console.log('Clicked button:', buttonId, 'Result:', result);
-  p1.choice = result;
-  PC.choice = cpuChose(pcChoiceArray);
   
-  const ch1 = p1.choice;
-  const ch2 = PC.choice;
-
-  // Compare the values and determine the winner
-  if (( ch1 === "Rock" && ch2 === "Paper") ||
-      ( ch1 === "Paper" && ch2 === "Scissor") ||
-      ( ch1 === "Scissor" && ch2 ===  "Rock")) {
-      
-      PC.score++;
-      cpuScore = document.getElementById("cpuScore");
-      cpuScoreText = PC.score;
-      cpuScore.textContent = cpuScoreText.toString(); 
-      refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
-      winnerMsg.textContent = "CPU Wins!";
+  if(gameOn === true)
+  {
+            const buttonId = event.target.id;
+            let result;
+          
+            switch (buttonId) {
+                case 'rockBtn':
+                    result = "Rock";
+                    break;
+                case 'paperBtn':
+                    result = "Paper";
+                    break;
+                case 'scissorsBtn':
+                    result = "Scissor";;
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+          
+            console.log('Clicked button:', buttonId, 'Result:', result);
+            p1.choice = result;
+            PC.choice = cpuChose(pcChoiceArray);
+            
+            const ch1 = p1.choice;
+            const ch2 = PC.choice;
+          
+            // Compare the values and determine the winner
+            if (( ch1 === "Rock" && ch2 === "Paper") ||
+                ( ch1 === "Paper" && ch2 === "Scissor") ||
+                ( ch1 === "Scissor" && ch2 ===  "Rock")) {
+                
+                PC.score++;
+                cpuScore = document.getElementById("cpuScore");
+                cpuScoreText = PC.score;
+                cpuScore.textContent = cpuScoreText.toString(); 
+                refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
+                winnerMsg.textContent = "CPU Wins!";
+            }
+           
+                
+                // return CPU; // Player 1 wins
+           else if (( ch1 ==="Paper" && ch2 === "Rock") ||
+                       ( ch1 ==="Scissor" && ch2 ==="Paper") ||
+                       ( ch1 === "Rock" && ch2 === "Scissor")) {
+                   p1.score++;
+                p1Score = document.getElementById("humanScore");
+                p1ScoreText = p1.score;
+                p1Score.textContent = p1ScoreText.toString();
+                refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
+                refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
+                winnerMsg.textContent = "PLAYER 1 WINS!";
+                
+                
+            } else {
+             refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
+              winnerMsg.textContent = "";
+               
+            }
   }
- 
-      
-      // return CPU; // Player 1 wins
- else if (( ch1 ==="Paper" && ch2 === "Rock") ||
-             ( ch1 ==="Scissor" && ch2 ==="Paper") ||
-             ( ch1 === "Rock" && ch2 === "Scissor")) {
-         p1.score++;
-      p1Score = document.getElementById("humanScore");
-      p1ScoreText = p1.score;
-      p1Score.textContent = p1ScoreText.toString();
-      refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
-      refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
-      winnerMsg.textContent = "PLAYER 1 WINS!";
-      
-      
-  } else {
-   refMsg.textContent = "Player 1 chose: " + ch1 + " and CPU chose: " + ch2;
-    winnerMsg.textContent = "";
-     
-  }
-     
+  compare(p1, PC);
 }
 function cpuChose(arr) {
   // Generate a random number between 1 and 3
@@ -91,7 +95,12 @@ const reset = () => {
 location.reload();
   
 }
+//compare the scores to decide final winner(first to score 5)4
 
+ function compare(p1, cpu) {
+ if (p1.score === 5) {gameOn = false;  refMsg.textContent = `${p1.name} is the Winner with a Score of ${p1.score}`;} 
+ else if (cpu.score === 5) {gameOn = false;  refMsg.textContent = `${cpu.name} is the Winner with a Score of ${cpu.score}`;}
+}
 
 // Add click event listeners to the buttons
 document.querySelector('#rockBtn').addEventListener('click', Player1Chose);
